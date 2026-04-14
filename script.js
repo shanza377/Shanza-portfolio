5da31db17bcff388042a25bf0fecc8d7ad032c2e
Shanza Hussain <qashan866@gmail.com>
2026-04-14 09:39:18 +0500

    My first portfolio with dark mode

---
+++ script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.getElementById('theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+if(document.body.classList.contains('dark-mode')) {
+    themeBtn.innerText = 'Light Mode';
+} else {
+    themeBtn.innerText = 'Dark Mode';
+}
+});
